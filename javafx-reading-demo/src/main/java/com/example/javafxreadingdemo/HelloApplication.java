b937b40f826135487be78e6d185c94d1a8817c0c
Manny <iamgoice@gmail.com>
2025-04-01 11:52:25 +1000

    Update HelloApplication.java

--- javafx-reading-demo/src/main/java/com/example/javafxreadingdemo/HelloApplication.java
+++ javafx-reading-demo/src/main/java/com/example/javafxreadingdemo/HelloApplication.java
@@ -1,16 +1,25 @@
 package com.example.javafxreadingdemo;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    public CharSequence userName;
+    public CharSequence password;
+
+
+
     @Override
     public void start(Stage stage) throws IOException {
         VBox root = new VBox();
@@ -18,11 +27,40 @@ public class HelloApplication extends Application {
         root.setSpacing(10.0);
         root.setFillWidth(false);
 
-        TextField textField = new TextField();
-        textField.setText("Enter something cool");
+
+
         Label label = new Label("Welcome to our project, Code Crafters!");
 
-        root.getChildren().addAll(label, textField);
+        VBox authentication  = new VBox();
+
+        TextField userNameField = new TextField();
+        Label userNameLabel = new Label("Username:");
+        userNameField.setText("blah blah blah");
+
+        Label passWordLabel = new Label("Password");
+        TextField passWordField = new TextField();
+        passWordField.setText("blah blah blah");
+
+        Button button = new Button("login!");
+
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                userName = userNameField.getCharacters();
+                password = passWordField.getCharacters();
+
+                System.out.println(userName);
+                System.out.println(password);
+            }
+        });
+
+
+        this.userName = userNameField.getCharacters();
+        authentication.getChildren().addAll(userNameLabel, userNameField, passWordLabel, passWordField, button );
+
+        root.getChildren().addAll(label, authentication);
+
+
 
         Scene scene = new Scene(root, 320, 180);
         stage.setScene(scene);
