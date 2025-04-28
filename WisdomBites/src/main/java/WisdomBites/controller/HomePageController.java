c6f9c4a05dd8cef0127eab1a770dc7dae8ee53f1
tariqdaly2708 <tariqdaly2708@gmail.com>
2025-04-28 23:21:00 +1000

    Added home page controller

---
+++ WisdomBites/src/main/java/WisdomBites/controller/HomePageController.java
@@ -0,0 +1,24 @@
+package WisdomBites.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class HomePageController {
+
+    @FXML
+    private ImageView cookieImage;
+
+    @FXML
+    private Button quotesButton, predictionsButton, todoButton, fortuneButton, progressButton, challengesButton;
+
+    @FXML
+    public void initialize() {
+        cookieImage.setImage(new Image(getClass().getResource("/close.cookie.png").toExternalForm()));
+
+        fortuneButton.setOnAction(e -> {
+            System.out.println("Get your fortune clicked!");
+        });
+    }
+}
