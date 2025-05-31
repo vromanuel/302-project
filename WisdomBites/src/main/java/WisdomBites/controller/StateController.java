290391b219eb7f1836099f404ec1da1933ab9d60
Manny <iamgoice@gmail.com>
2025-05-31 21:15:51 +1000

    GOSH

--- WisdomBites/src/main/java/WisdomBites/controller/StateController.java
+++ WisdomBites/src/main/java/WisdomBites/controller/StateController.java
@@ -3,12 +3,11 @@ package WisdomBites.controller;
 import WisdomBites.model.User;
 
 public class StateController {
+    // This class stores the states
+
+
     // The user that is logged in
     public static User currentUser;
 
     public static void setCurrentUser(User user) {currentUser = user;}
-
-    public static int getUserId() {
-        return currentUser != null ? currentUser.getId() : -1; // or throw an error if preferred
-    }
 }
